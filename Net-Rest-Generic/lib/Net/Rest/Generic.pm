d62ced5fabf469ff6bb5a320c70b9906a3250e50
Sebastian Green-Husted <ricecake@tfm.nu>
2013-10-30 23:42:38 -0400

    move code out of dummy file, into proper class location.

--- Net-Rest-Generic/lib/Net/Rest/Generic.pm
+++ Net-Rest-Generic/lib/Net/Rest/Generic.pm
@@ -4,6 +4,7 @@ use 5.006;
 use strict;
 use warnings FATAL => 'all';
 
+use Want;
 =head1 NAME
 
 Net::Rest::Generic - The great new Net::Rest::Generic!
@@ -35,19 +36,53 @@ if you don't export anything, such as for a purely object-oriented module.
 
 =head1 SUBROUTINES/METHODS
 
-=head2 function1
-
 =cut
 
-sub function1 {
+sub new {
+	my ($class, %params) = @_;
+	my %defaults = (
+		mode   => 'get',
+		scheme => 'http',
+	);
+	my $self = {
+		_chain  => [],
+		_params => \%params
+	};
+	map { $self->{$_} = delete $self->{_params}{$_} } grep { defined($self->{_params}{$_}) } qw(mode scheme host port base);
+	while (my ($k, $v) = each %defaults) {
+		$self->{$k} ||= $v;
+	}
+	return bless $self, $class;
 }
 
-=head2 function2
 
-=cut
+sub AUTOLOAD {
+	my $self = shift;
 
-sub function2 {
+	our $AUTOLOAD;
+	my ($key) = $AUTOLOAD =~ /.*::([\w_]+)/o;
+	return if ($key eq 'DESTROY');
+
+	if (want('OBJECT') || want('VOID')) {
+		push @{ $self->{chain} }, $key;
+		if (@_ == 1) {
+			push @{ $self->{chain} }, $_[0];
+		}
+		return $self;
+	}
+
+	my $args = ref($_[0]) ? $_[0] : {@_};
+
+	my $url = join('/', @{ $self->{chain} }, $key);
+	my $method = $self->{mode}   || '';
+	my $scheme = $self->{scheme} || '';
+	my $host   = $self->{host}   || '';
+	my $port = $self->{port} ? ":" . $self->{port}       : '';
+	my $base = $self->{base} ? "/" . $self->{base} . "/" : '';
+
+	return "$method $scheme://$host$port$base$url";
 }
+
 
 =head1 AUTHOR
 
