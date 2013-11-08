ac57f646d4347df2a39a5314761b97af5d261827
Sebastian Green-Husted <ricecake@tfm.nu>
2013-11-07 20:47:28 -0500

    second attempt at diddling the auth stuff.

--- Net-Rest-Generic/lib/Net/Rest/Generic/Utility.pm
+++ Net-Rest-Generic/lib/Net/Rest/Generic/Utility.pm
@@ -5,14 +5,26 @@ use strict;
 use warnings FATAL => 'all';
 
 use LWP::UserAgent;
+use HTTP::Request::Common;
 
 sub _doRestCall {
         my ($api, $method, $url, $args) = @_;
+        $method = uc($method);
         $args ||= {};
         $api->{ua} ||= LWP::UserAgent->new;
-        $api->{ua}->credentials($url->host_port(), $api->{authorization_basic}{username}, $api->{authorization_basic}{password})
-                if $api->{authorization_basic}{username};
-        return $api->{ua}->$method($url, %{$api->{_params}}, %{$args});
+        my $ua = $api->{ua};
+        my @parameters = (%{$api->{_params}}, %{$args});
+        my @suff = $ua->_process_colonic_headers(\@parameters,1);
+        {
+                no strict qw(refs);
+                my $request = &{"HTTP::Request::Common::${method}"}( @parameters );
+                $request->authorization_basic(
+                        $api->{authorization_basic}{username},
+                        $api->{authorization_basic}{password}
+                ) if $api->{authorization_basic}{username};
+
+                return $ua->request( $request, @suff );
+        }
 }
 
 sub _validateInput {
