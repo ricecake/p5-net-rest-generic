b41690a960f518a00d72a6bf8ba9db717e6c4910
Shane Utt <shaneutt@linux.com>
2013-11-21 03:43:19 -0500

    Added POD to Error.pm

--- Net-Rest-Generic/lib/Net/Rest/Generic/Error.pm
+++ Net-Rest-Generic/lib/Net/Rest/Generic/Error.pm
@@ -4,11 +4,42 @@ use 5.006;
 use strict;
 use warnings FATAL => 'all';
 
+=head1 NAME
+
+Net::Rest::Generic::Error - Error handling for Net::Rest::Generic
+
+=head1 SUBROUTINES/METHODS
+
+=head2 throw()
+
+Used like ->new to give an error object.
+
+=head3 USAGE
+
+	ARGUMENT	DESCRIPTION
+	---
+	type		Describes the variety of failure occurred. Potential
+			values could be 'perl error', or basic http error
+			codes such as '404', '500', e.t.c.
+			~ Default: 'fail'
+
+	category	Describes what actually failed. Potential values
+			could be 'http request', 'http auth', or something
+			specific to your application like 'foobar api error'.
+			~ Default: 'object' (assumes something is wrong with
+			  Net::Rest::Generic unless told otherwise)
+
+	message		Describes the specifics of what actually occurred for
+			a user to read.
+			~ Default: 'unknown'
+	---
+
+=cut
+
 #use ->throw like ->new to give an error object
 sub throw {
 	my $class = shift;
 	my %args = ref($_[0]) ? %{$_[0]} : @_;
-	#assume its something wrong with Net::Rest::Generic unless told otherwise.
 	$args{type}     = $args{type}     ? $args{type}     : 'fail';
 	$args{category} = $args{category} ? $args{category} : 'object';
 	$args{message}  = $args{message}  ? $args{message}  : 'unknown';
