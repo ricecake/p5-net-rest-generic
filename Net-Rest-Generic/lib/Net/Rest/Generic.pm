31d0257300e8415c4acc6b03de36ab7cd0762987
Shane Utt <shane@helios>
2013-11-24 14:36:40 -0500

    Notes for 06

--- Net-Rest-Generic/lib/Net/Rest/Generic.pm
+++ Net-Rest-Generic/lib/Net/Rest/Generic.pm
@@ -16,11 +16,11 @@ Net::Rest::Generic - A tool for generically interacting with restfull (or restli
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
