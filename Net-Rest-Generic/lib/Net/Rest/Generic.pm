32ae7ac69df38fd7f64772b98fc1daa0ec033139
Shane Utt <shaneutt@linux.com>
2014-03-25 20:37:42 -0400

    Updated the Makefile to include some build necessities

--- Net-Rest-Generic/lib/Net/Rest/Generic.pm
+++ Net-Rest-Generic/lib/Net/Rest/Generic.pm
@@ -16,11 +16,11 @@ Net::Rest::Generic - A tool for generically interacting with restfull (or restli
 
 =head1 VERSION
 
-Version 0.08
+Version 0.09
 
 =cut
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 =head1 SYNOPSIS
 
